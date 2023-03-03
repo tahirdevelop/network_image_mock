bccffd7228a0ed1570696fdbd28aec0706aa785a
Tahir <46454929+tahirdevelop@users.noreply.github.com>
2023-03-03 20:15:50 +0400

    updated imports

--- lib/network_image_mock.dart
+++ lib/network_image_mock.dart
@@ -1,3 +1,5 @@
 library network_image_mock;
 
 export './src/network_image_mock.dart' show mockNetworkImagesFor;
+export './src/network_image_mock.dart' show mockDefaultNetworkSvg;
+export './src/network_image_mock.dart' show mockDefaultNetworkImage;
